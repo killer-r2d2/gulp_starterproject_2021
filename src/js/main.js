e92644d115a57fae1d58a1d6f575390e19cc497a
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-07-11 18:42:56 +0200

    comment for toggler function hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
+// select the hamburger box with a variable
 const menutoggler = document.querySelector(".headerSection__hamburgerBox");
 menutoggler.addEventListener('click', toggler);
 
+// toggler function
 function toggler() {
+    // select the hamburger with the id
     const hamburger = document.querySelector("#hamburger");
+    // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
     hamburger.classList.toggle("headerSection__hamburger-close");
+    // add the headerSection__hamburger with toggle to the headerSection__hamburger-close
     hamburger.classList.toggle("headerSection__hamburger");
 }
