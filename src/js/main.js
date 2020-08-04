c25e3f62b1448910ec126524f9861e5f4c14d986
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-08-04 09:20:44 +0200

    implement grid for title section

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,29 @@
-// select the hamburger box with a variable
-const menutoggler = document.querySelector(".headerSection__hamburgerBox");
-menutoggler.addEventListener('click', togglermenu);
-
-// togglermenu function
-function togglermenu() {
-    // select the hamburger with the id
-    const hamburger = document.querySelector("#hamburger");
-    // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
-    hamburger.classList.toggle("headerSection__hamburger-close");
-    hamburger.classList.toggle("headerSection__hamburger");
-    const layer = document.querySelector("#navSection");
-    layer.classList.toggle("navSection--show");
-    layer.classList.toggle("navSection");
-    headerSection.classList.toggle("headerSection--changeColor");
-    // headerSection.classList.toggle("headerSection");
-    const ticket = document.querySelector("#navigation__ticketkauf");
-    ticket.classList.toggle("navigation__ticket--white");
-    ticket.classList.toggle("navigation__ticket");
-    const login = document.querySelector("#navigation_login");
-    login.classList.toggle("navigation__login--white");
-    login.classList.toggle("navigation__login");
-    const like = document.querySelector("#detail_like_filled");
-    like.classList.toggle("navigation__like");
-    like.classList.toggle("navigation__like--white");
-}
+// // select the hamburger box with a variable
+// const menutoggler = document.querySelector(".headerSection__hamburgerBox");
+// menutoggler.addEventListener('click', togglermenu);
+//
+// // togglermenu function
+// function togglermenu() {
+//     // select the hamburger with the id
+//     const hamburger = document.querySelector("#hamburger");
+//     // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
+//     hamburger.classList.toggle("headerSection__hamburger-close");
+//     hamburger.classList.toggle("headerSection__hamburger");
+//     const layer = document.querySelector("#navSection");
+//     layer.classList.toggle("navSection--show");
+//     layer.classList.toggle("navSection");
+//     headerSection.classList.toggle("headerSection--changeColor");
+//     // headerSection.classList.toggle("headerSection");
+//     const ticket = document.querySelector("#navigation__ticketkauf");
+//     ticket.classList.toggle("navigation__ticket--white");
+//     ticket.classList.toggle("navigation__ticket");
+//     const login = document.querySelector("#navigation_login");
+//     login.classList.toggle("navigation__login--white");
+//     login.classList.toggle("navigation__login");
+//     const like = document.querySelector("#detail_like_filled");
+//     like.classList.toggle("navigation__like");
+//     like.classList.toggle("navigation__like--white");
+// }
 
 
 // select the login Icon with a variable
