aec3b3ac34048466db2f9b9b683c22fddb9d0d0d
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-08-04 12:01:58 +0200

    hover for titles

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,51 @@
-// // select the hamburger box with a variable
-// const menutoggler = document.querySelector(".headerSection__hamburgerBox");
-// menutoggler.addEventListener('click', togglermenu);
-//
+// select the hamburger box with a variable
+const menutoggler = document.querySelector(".headerSection__hamburgerBox");
+menutoggler.addEventListener('click', togglermenu);
+
+
 // // togglermenu function
-// function togglermenu() {
-//     // select the hamburger with the id
-//     const hamburger = document.querySelector("#hamburger");
-//     // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
-//     hamburger.classList.toggle("headerSection__hamburger-close");
-//     hamburger.classList.toggle("headerSection__hamburger");
-//     const layer = document.querySelector("#navSection");
-//     layer.classList.toggle("navSection--show");
-//     layer.classList.toggle("navSection");
-//     headerSection.classList.toggle("headerSection--changeColor");
-//     // headerSection.classList.toggle("headerSection");
-//     const ticket = document.querySelector("#navigation__ticketkauf");
-//     ticket.classList.toggle("navigation__ticket--white");
-//     ticket.classList.toggle("navigation__ticket");
-//     const login = document.querySelector("#navigation_login");
-//     login.classList.toggle("navigation__login--white");
-//     login.classList.toggle("navigation__login");
-//     const like = document.querySelector("#detail_like_filled");
-//     like.classList.toggle("navigation__like");
-//     like.classList.toggle("navigation__like--white");
-// }
+function togglermenu() {
+
+
+    // select the hamburger with the id
+    const hamburger = document.querySelector("#hamburger");
+    // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
+    hamburger.classList.toggle("headerSection__hamburger-close");
+    hamburger.classList.toggle("headerSection__hamburger");
+    const layer = document.querySelector("#navSection");
+    layer.classList.toggle("navSection--show");
+    layer.classList.toggle("navSection");
+    headerSection.classList.toggle("headerSection--changeColor");
+    // headerSection.classList.toggle("headerSection");
+    const ticket = document.querySelector("#navigation__ticketkauf");
+    ticket.classList.toggle("navigation__ticket--white");
+    ticket.classList.toggle("navigation__ticket");
+    const login = document.querySelector("#navigation_login");
+    login.classList.toggle("navigation__login--white");
+    login.classList.toggle("navigation__login");
+    const like = document.querySelector("#detail_like_filled");
+    like.classList.toggle("navigation__like");
+    like.classList.toggle("navigation__like--white");
+    const text = document.querySelector(".headerSection__text");
+    // change the text
+    if (text.innerHTML === "Menu") {
+        text.innerHTML = "Schliessen";
+    } else {
+        text.innerHTML = "Menu";
+    }
+    // change the color of the text
+    if (text.style.color = 'black') {
+        text.style.color = 'white';
+    } else {
+        text.style.color = 'black';
+    }
+
+}
 
 
 // select the login Icon with a variable
 const logintoggler = document.querySelector(".headerSection__IconLogin");
-logintoggler.addEventListener('click', togglerlogin );
+logintoggler.addEventListener('click', togglerlogin);
 
 // togglerlogin function
 function togglerlogin() {
