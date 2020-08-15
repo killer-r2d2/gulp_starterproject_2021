f8aa2e6718361b8088d8916c7932bf277a0fcb4f
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-08-15 14:43:26 +0200

    container--A responsive til 800px ok

--- src/js/main.js
+++ src/js/main.js
@@ -127,6 +127,8 @@ if (knowledgeWrappper) {
 
 // show function for accordionBox with delegate approach
 function showMe(event) {
+    // the event.target represents the choose button and whatched if there is (contains) a choose button with
+    // the class (classList) knowledgeSection__choose.
     if(event.target.classList.contains("knowledgeSection__choose")) {
         console.log(event.target.parentNode.parentNode.querySelector('.knowledgeSection__lowerContainer'));
         const showInfo = event.target.parentNode.parentNode.querySelector('.knowledgeSection__lowerContainer');
