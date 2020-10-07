23002ea23eecc0da5dac8dcda789d364b4cb007d
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-10-07 21:03:35 +0200

    outsource the loggin-toggler

--- src/js/main.js
+++ src/js/main.js
@@ -5,29 +5,12 @@ import * as listTilesView from './list-tiles-view.js';
 import * as AccordionDetailsite from './accordion-detailsite.js';
 import * as AccordionMagazineSection from './accordion-magazineSection.js';
 import * as NoticeStarDetailsite from './noticeStar-detailsite.js';
+import * as LoginToggler from './loggin-toggler.js';
 import {initMap} from './map.js';
 window.initMap = initMap;
 
 
-// select the login Icon with a variable
-const logintoggler = document.querySelector(".headerSection__IconLogin");
-logintoggler.addEventListener('click', togglerlogin);
 
-// togglerlogin function
-function togglerlogin() {
-    const loginshow = document.querySelector('#loginSection__containter');
-    loginshow.classList.toggle("loginSection__containter--open");
-    const logincolor = document.querySelector("#navigation_login");
-    logincolor.classList.toggle("navigation__login--black");
-    const changeColor = document.querySelector(".headerSection__IconLoginText");
-    changeColor.classList.toggle("headerSection__IconLoginText--color");
-}
-
-// add stopPropagation to the login container.
-const loginview = document.querySelector('.loginSection__containter');
-loginview.addEventListener('click', function (event) {
-    event.stopPropagation();
-})
 
 
 // start with slider
