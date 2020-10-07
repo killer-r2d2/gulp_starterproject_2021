831e157fa570c26ea4e417edc51924e06761f798
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-10-07 20:49:44 +0200

    outsource the list-tiles-view

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import * as TicketCounter from './ticket-counter.js';
 import * as Menu from './menu-toggler.js';
 import * as NoticeStarOverview from './noticeStar-overviewFestivals.js';
+import * as listTilesView from './list-tiles-view.js';
 import {initMap} from './map.js';
 
 window.initMap = initMap;
@@ -35,36 +36,7 @@ loginview.addEventListener('click', function (event) {
 
 
 
-// Change the view between list and tiles of the festivals site
 
-// select the list icon with a variable
-const viewList = document.querySelector(".overviewSection__labelContainer--list");
-// we check if the variable viewList (class .overviewSection__labelContainer--list) exist
-if (viewList) {
-    viewList.addEventListener('click', listView);
-}
-
-// listView function
-function listView() {
-    console.log("hallo hölle");
-    const listShow = document.querySelector(".overviewSection__cardContainerTiles");
-    // listShow.classList.remove("overviewSection__cardContainerTiles");
-    listShow.classList.add("overviewSection__cardContainerlist");
-}
-
-// select the list icon with a variable
-const viewTiles = document.querySelector(".overviewSection__labelContainer--tiles");
-// we check if the variable viewTiles(class .overviewSection__labelContainer--tiles) exist
-if (viewTiles) {
-    viewTiles.addEventListener('click', tilesView);
-}
-
-// tilesView function
-function tilesView() {
-    console.log("hallo frucht");
-    const tilesShow = document.querySelector(".overviewSection__cardContainerTiles");
-    tilesShow.classList.remove("overviewSection__cardContainerlist");
-}
 
 
 // select the accordionContainer
