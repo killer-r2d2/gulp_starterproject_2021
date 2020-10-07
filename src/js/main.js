6fac093c12c674ce4104673fb966737f53c2ad85
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-10-07 20:46:54 +0200

    outsource the noticeStar-overviewFestivals

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import * as TicketCounter from './ticket-counter.js';
 import * as Menu from './menu-toggler.js';
+import * as NoticeStarOverview from './noticeStar-overviewFestivals.js';
 import {initMap} from './map.js';
 
 window.initMap = initMap;
@@ -210,23 +211,7 @@ function showMeMore(event) {
 
 
 
-// start notice star overview-festivals
 
-// admit function with delegate approach
-const clickStar = document.querySelector('.overviewSection');
-// we check if the variable clickStar (class .overviewSection) exist
-if(clickStar) {
-    clickStar.addEventListener('click', noticeFestival);
-}
-
-function noticeFestival(event) {
-    if(event.target.classList.contains('overviewSection__noticeContainer')); {
-        const whiteStar = event.target.parentNode.querySelector('.overviewSection__noticeStarWhite');
-        whiteStar.classList.toggle('overviewSection__noticeStarWhite--hidde');
-        const blackStar = event.target.parentNode.querySelector('.overviewSection__noticeStarBlack');
-        blackStar.classList.toggle('overviewSection__noticeStarBlack--show');
-    }
-}
 
 // start notice star detailsite
 const clickStar2 = document.querySelector('.textSection__noticeContainer--1');
