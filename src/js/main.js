c77e9396daed829c8fbf2e81e5f0f334a549944d
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-10-07 20:42:01 +0200

    outsource the menu-toggler

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import * as TicketCounter from './ticket-counter.js';
+import * as Menu from './menu-toggler.js';
 import {initMap} from './map.js';
 
 window.initMap = initMap;
@@ -7,41 +8,7 @@ window.initMap = initMap;
 
 
 
-// select the hamburger box with a variable
-const menutoggler = document.querySelector(".headerSection__hamburgerBox");
-menutoggler.addEventListener('click', togglermenu);
 
-// // togglermenu function
-function togglermenu() {
-    // select the hamburger with the id
-    const hamburger = document.querySelector("#hamburger");
-    // add the headerSection__hamburger-close with toggle to the headerSection__hamburger
-    hamburger.classList.toggle("headerSection__hamburger-close");
-    hamburger.classList.toggle("headerSection__hamburger");
-    const layer = document.querySelector("#navSection");
-    layer.classList.toggle("navSection--show");
-    const headerSection = document.querySelector(".headerSection")
-    headerSection.classList.toggle("headerSection--changeColor");
-    const ticket = document.querySelector("#navigation__ticketkauf");
-    ticket.classList.toggle("navigation__ticket--white");
-    const login = document.querySelector("#navigation_login");
-    login.classList.toggle("navigation__login--white");
-    const like = document.querySelector("#detail_like_filled");
-    like.classList.toggle("navigation__like--white");
-    const headerLogoBlack = document.querySelector("#flLogo");
-    headerLogoBlack.classList.toggle("headerSection__flLogo--hide");
-    const headerLogoWhite = document.querySelector("#flLogoWhite");
-    headerLogoWhite.classList.toggle("headerSection__flLogoWhite--show");
-    const text = document.querySelector(".headerSection__text");
-    // change the text
-    if (text.innerHTML === "Menu") {
-        text.innerHTML = "Schliessen";
-        text.style.color = "white";
-    } else {
-        text.innerHTML = "Menu";
-        text.style.color = "black"
-    }
-}
 
 
 // select the login Icon with a variable
