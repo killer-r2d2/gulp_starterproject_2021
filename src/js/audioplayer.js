100247ded2317fba7d3676d690d7e4e4a7d643ca
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-10-26 20:50:31 +0100

    playMusic function in progress

--- src/js/audioplayer.js
+++ src/js/audioplayer.js
@@ -1,9 +1,17 @@
-const musicBox = document.querySelector(".discoverySection__imgBox");
+const musicBox = document.querySelector(".discoverySection__imgContainer");
 musicBox.addEventListener('click', playMusic);
 
-function playMusic() {
-    const playBtn = document.querySelector('.discoverySection__play');
-    playBtn.classList.toggle("discoverySection__play--hide");
-    const stopBtn = document.querySelector('.discoverySection__stop');
-    stopBtn.classList.toggle("discoverySection__stop--show");
+function playMusic(event) {
+    if(event.target.classList.contains("discoverySection__play")) {
+        const hideBtn = event.target.parentNode.querySelector('.discoverySection__play');
+        hideBtn.classList.toggle("discoverySection__play--hide");
+        const stopBtn = event.target.parentNode.querySelector('.discoverySection__stop');
+        stopBtn.classList.toggle("discoverySection__stop--show");
+    }
+
+    // const playBtn = document.querySelector('.discoverySection__play');
+    // playBtn.classList.toggle("discoverySection__play--hide");
+    // const stopBtn = document.querySelector('.discoverySection__stop');
+    // stopBtn.classList.toggle("discoverySection__stop--show");
 }
+
