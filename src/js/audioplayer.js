0c119db6d50ff1498f466ac761b2a43244028498
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-04 20:38:46 +0100

    playMusic function toggle play and stop

--- src/js/audioplayer.js
+++ src/js/audioplayer.js
@@ -2,16 +2,15 @@ const musicBox = document.querySelector(".discoverySection__imgContainer");
 musicBox.addEventListener('click', playMusic);
 
 function playMusic(event) {
-    if(event.target.classList.contains("discoverySection__play")) {
+    if(event.target.classList.contains("discoverySection__contBox")) {
+        console.log('hoi');
         const hideBtn = event.target.parentNode.querySelector('.discoverySection__play');
         hideBtn.classList.toggle("discoverySection__play--hide");
         const stopBtn = event.target.parentNode.querySelector('.discoverySection__stop');
         stopBtn.classList.toggle("discoverySection__stop--show");
     }
-
-    // const playBtn = document.querySelector('.discoverySection__play');
-    // playBtn.classList.toggle("discoverySection__play--hide");
-    // const stopBtn = document.querySelector('.discoverySection__stop');
-    // stopBtn.classList.toggle("discoverySection__stop--show");
 }
+
+
+
 
