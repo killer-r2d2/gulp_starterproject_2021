78cc9841b603166cbd917b44e3c642f95e3813cd
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-07 20:16:01 +0100

    fix toggle bug audioplayer.js

--- src/js/audioplayer.js
+++ src/js/audioplayer.js
@@ -1,18 +1,14 @@
-// const musicBox = document.querySelector(".discoverySection__imgContainer");
-// musicBox.addEventListener('click', playMusic);
-//
-// function playMusic(event) {
-//     if(event.target.classList.contains("discoverySection__contBox")) {
-//         const hideBtn = event.target.parentNode.querySelector('.discoverySection__play');
-//         hideBtn.classList.toggle("discoverySection__play--hide");
-//         const stopBtn = event.target.parentNode.querySelector('.discoverySection__stop');
-//         stopBtn.classList.toggle("discoverySection__stop--show");
-//         // if(stopBtn.classList.contains("discoverySection__stop--show")) {
-//         //     console.log("erfolg");
-//         //
-//         // }
-//     }
-// }
+const musicBox = document.querySelector(".discoverySection__imgContainer");
+musicBox.addEventListener('click', playMusic);
+
+function playMusic(event) {
+    if(event.target.classList.contains("discoverySection__musicBtn")) {
+        const hideBtn = event.target.parentNode.querySelector('.discoverySection__musicBtn--play');
+        hideBtn.classList.toggle("discoverySection__musicBtn--hide");
+        const stopBtn = event.target.parentNode.querySelector('.discoverySection__musicBtn--stop');
+        stopBtn.classList.toggle("discoverySection__musicBtn--show");
+    }
+}
 
 
 
