2e60d4967b9acb52dda87b78233f06ea187948ad
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-11 16:03:27 +0100

    start with the lineupToggle function in progess

--- src/js/lineuptoggler.js
+++ src/js/lineuptoggler.js
@@ -3,7 +3,12 @@ if(lineUp) {
     lineUp.addEventListener('click', lineupToggle);
 }
 
-function lineupToggle() {
+function lineupToggle(event) {
     console.log("funktioniert");
+    if(event.target.classList.contains("lineUpSection__btn--1")) {
+        const scheduleFriday = document.querySelector(".lineUpSection__scheduleContainer");
+        scheduleFriday.classList.toggle("lineUpSection__scheduleContainer--show");
+    }
+
 
 }
