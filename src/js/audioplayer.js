630e388a48af6512bb65be5f582fb53cce0726e7
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-19 15:58:54 +0100

    add an mp3 to the audioplayer.js

--- src/js/audioplayer.js
+++ src/js/audioplayer.js
@@ -1,4 +1,12 @@
+const sound = document.querySelector(".sound");
+
+//function beePer(event) {
+  //  console.log("playAlter");
+   // sound.play();
+//}
+
 const musicBox = document.querySelector(".discoverySection__imgContainer");
+
 if(musicBox) {
     musicBox.addEventListener('click', playMusic2);
 }
@@ -16,6 +24,7 @@ function playMusic2(event) {
     if (event.target.classList.contains("discoverySection__musicBtn--play")) {
         isPlaying = "true";
     };
+//    beePer();
     stopMusic();// remove the class .discoverySection__musicBtn--play
     if (isPlaying === "") {
         const playBtn = event.target.parentNode.querySelector('.discoverySection__musicBtn');
