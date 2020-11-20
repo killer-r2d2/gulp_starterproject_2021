6b64484870e7c26a0863bbb33e2749e3dc9d9397
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-20 08:57:41 +0100

    remove class .lineUpSection__scheduleContainer

--- src/js/lineuptoggler.js
+++ src/js/lineuptoggler.js
@@ -3,9 +3,18 @@ if(lineUp) {
     lineUp.addEventListener('click', lineupToggle);
 }
 
+function hideLineup() {
+
+    console.log('toggleWorks');
+    let statuslineup = document.querySelectorAll(".lineUpSection__scheduleContainer");
+    for (let i = 0; i < statuslineup.length; i++) {
+        statuslineup[i].classList.remove('lineUpSection__scheduleContainer--show');
+    }
+}
+
 function lineupToggle(event) {
+    hideLineup();
     if (event.target.classList.contains("lineUpSection__btn")) {
-        console.log(event.target.classList.contains("lineUpSection__btn"));
         const schedule = event.target.parentNode.querySelector('.lineUpSection__scheduleContainer');
         schedule.classList.toggle('lineUpSection__scheduleContainer--show');
 
