43b960f61ac905d26b1e2e4dee3b32d85b505143
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-24 18:46:33 +0100

    breadcrumb in progress

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -1,1 +1,12 @@
-// se
+// select the overviewSection__breadCrumbContainer
+const breadBox = document.querySelector(".overviewSection__breadCrumbContainer");
+// we check if the variable breadBox = document.querySelector(".overviewSection__breadCrumbContainer") exist.
+if (breadBox) {
+    breadBox.addEventListener('click', showdropdown);
+}
+
+function showdropdown() {
+    console.log("Bier");
+    const dropDown = document.querySelector('.overviewSection__dropitem--hide');
+    dropDown.classList.toggle('overviewSection__dropitem--hide');
+}
