bc24da13c01b4b5ee62151b8c3e2847588dc3949
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-25 11:27:30 +0100

    dropedown works

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -1,12 +1,12 @@
 // select the overviewSection__breadCrumbContainer
-const breadBox = document.querySelector(".overviewSection__breadCrumbContainer");
+const arrowDown = document.querySelector(".overviewSection__dropdownarrow");
 // we check if the variable breadBox = document.querySelector(".overviewSection__breadCrumbContainer") exist.
-if (breadBox) {
-    breadBox.addEventListener('click', showdropdown);
+if (arrowDown) {
+    arrowDown.addEventListener('click', showdropdown);
 }
 
 function showdropdown() {
     console.log("Bier");
-    const dropDown = document.querySelector('.overviewSection__dropitem--hide');
-    dropDown.classList.toggle('overviewSection__dropitem--hide');
+    const dropDown = document.querySelector('.overviewSection__festivaldrope');
+    dropDown.classList.toggle('overviewSection__festivaldrope--show');
 }
