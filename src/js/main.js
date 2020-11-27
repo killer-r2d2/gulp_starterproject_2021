4ff5d9d74d15906aac6f547f70fe88c23d757598
Roger Killer <57812837+killer-r2d2@users.noreply.github.com>
2020-11-27 15:01:07 +0100

    update README.md

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,4 @@
-import * as TicketCounter from './ticket-counter.js';
-import * as Menu from './menu-toggler.js';
-import * as NoticeStarOverview from './noticeStar-overviewFestivals.js';
-import * as listTilesView from './list-tiles-view.js';
-import * as AccordionDetailsite from './accordion-detailsite.js';
-import * as AccordionMagazineSection from './accordion-magazineSection.js';
-import * as NoticeStarDetailsite from './noticeStar-detailsite.js';
-import * as LoginToggler from './loggin-toggler.js';
-import * as AudioPlayer from './audioplayer.js';
-import * as LineupToggler from './lineuptoggler.js';
-import * as NoticeHeart from './noticeHeart.js';
-import * as BreadCrumb from './breadcrumb.js';
-import {initMap} from './map.js';
-window.initMap = initMap;
+import * as TestJs from './test.js';
 
 
 
